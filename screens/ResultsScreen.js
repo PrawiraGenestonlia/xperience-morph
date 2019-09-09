9f8c9a03e373c260c994b2c85b7bda700fcda65a
PrawiraGenestonlia <prawira.96@gmail.com>
2019-09-09 18:38:28 +0800

    .

---
+++ screens/ResultsScreen.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import {
+  ScrollView, StyleSheet, View, Image, Text,
+} from 'react-native';
+
+export default function ResultsScreen() {
+  return (
+    <ScrollView style={styles.container}>
+      <View>
+        <Text>Results Screen</Text>
+      </View>
+    </ScrollView>
+  )
+}
+
+ResultsScreen.navigationOptions = {
+  title: 'Morph',
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
